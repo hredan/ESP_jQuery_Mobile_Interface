68d2428ab56df94dab1669de269487fc03dc9e79
hredan <hredan@sourcecode3d.de>
2021-10-22 07:41:23 +0200

    make code c++ google style conform

--- ESP_jQuery_Mobile_Interface.ino
+++ ESP_jQuery_Mobile_Interface.ino
@@ -1,63 +1,76 @@
+/*
+  ESP_jQuery_Mobile_Interface.ino
+
+  ESP_jQuery_Mobile_Interface.ino is an example how to switch on a board LED via web interface for ESP8266 and ESP32. 
+
+  Information and contribution at https://github.com/hredan/ESP_jQuery_Mobile_Interface.
+
+  Copyright (C) 2021  André Herrmann (hredan)
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 
 #ifdef ESP32
-  #include <WiFi.h>           //https://github.com/espressif/arduino-esp32
-  #include <LITTLEFS.h>
-  #define MYFS LITTLEFS
-  const int LED_BUILTIN = 2;
+#include <LITTLEFS.h>
+#include <WiFi.h>  // https://github.com/espressif/arduino-esp32
+#define MYFS LITTLEFS
+const int LED_BUILTIN = 2;
 #else
-  #include <ESP8266WiFi.h>    //https://github.com/esp8266/Arduino
-  #include "LittleFS.h"
-  #define MYFS LittleFS
+#include <ESP8266WiFi.h>  // https://github.com/esp8266/Arduino
+
+#include "LittleFS.h"
+#define MYFS LittleFS
 #endif
 #include "handleWebpage.h"
 
-
-//declaration of needed instances
+// declaration of needed instances
 HandleWebpage *handleWebpage;
-   
-void setup()
-{
-    Serial.begin(115200);
-    Serial.print("\n");
 
-    pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
-    digitalWrite(LED_BUILTIN, HIGH);  // set led off
-    
-    //init little filesystem
-    if (MYFS.begin())
-    {
-        Serial.print("LittleFS started successfully\n");
-        #ifdef ESP32
-          Serial.printf("FS total Bytes %d\n", MYFS.totalBytes());
-          Serial.printf("FS used Bytes %d\n", MYFS.usedBytes());
-        #else
-          FSInfo fs_info;
-          LittleFS.info(fs_info);
-          Serial.printf("FS total Bytes %d\n", fs_info.totalBytes);
-          Serial.printf("FS used Bytes %d\n", fs_info.usedBytes);
-        #endif
-    }
-    else
-    {
-        Serial.print("Error: Could not start LittleFS!\n");
-    }
+void setup() {
+  Serial.begin(115200);
+  Serial.print("\n");
 
-    
+  pinMode(LED_BUILTIN, OUTPUT);  // Initialize the LED_BUILTIN pin as an output
+  digitalWrite(LED_BUILTIN, HIGH);  // set led off
 
-    WiFi.mode(WIFI_AP);
-    WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-    WiFi.softAP("SleepUinoPreDev");
+  // init little filesystem
+  if (MYFS.begin()) {
+    Serial.print("LittleFS started successfully\n");
+#ifdef ESP32
+    Serial.printf("FS total Bytes %d\n", MYFS.totalBytes());
+    Serial.printf("FS used Bytes %d\n", MYFS.usedBytes());
+#else
+    FSInfo fs_info;
+    LittleFS.info(fs_info);
+    Serial.printf("FS total Bytes %d\n", fs_info.totalBytes);
+    Serial.printf("FS used Bytes %d\n", fs_info.usedBytes);
+#endif
+  } else {
+    Serial.print("Error: Could not start LittleFS!\n");
+  }
 
-    handleWebpage = new HandleWebpage();
-    handleWebpage->setupHandleWebpage();
-    
-    // if DNSServer is started with "*" for domain name, it will reply with
-    // provided IP to all DNS request
-    dnsServer.start(DNS_PORT, "*", apIP);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  WiFi.softAP("SleepUinoPreDev");
+
+  handleWebpage = new HandleWebpage();
+  handleWebpage->setupHandleWebpage();
+
+  // if DNSServer is started with "*" for domain name, it will reply with
+  // provided IP to all DNS request
+  dnsServer.start(DNS_PORT, "*", apIP);
 }
 
-void loop()
-{   
-    dnsServer.processNextRequest();
-    handleWebpage->handleClient();
+void loop() {
+  dnsServer.processNextRequest();
+  handleWebpage->handleClient();
 }
--- handleWebpage.ino
+++ handleWebpage.ino
@@ -1,113 +1,132 @@
+/*
+  handleWebpage.ino
+
+  handleWebpage.ino is used by ESP_jQuery_Mobile_Interface.ino.
+
+  Information and contribution at https://www.sleepuino.sourcecode3d.de/.
+
+  Copyright (C) 2021  André Herrmann (hredan)
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include "handleWebpage.h"
 
 #ifdef ESP32
-  WebServer* HandleWebpage::_webServer = nullptr;
+WebServer* HandleWebpage::_webServer = nullptr;
 #else
-  ESP8266WebServer* HandleWebpage::_webServer = nullptr;
+ESP8266WebServer* HandleWebpage::_webServer = nullptr;
 #endif
 
+HandleWebpage::HandleWebpage() {
+#ifdef ESP32
+  _webServer = new WebServer(80);
+#else
+  _webServer = new ESP8266WebServer(80);
+#endif
+}
 
-
-HandleWebpage::HandleWebpage()
-{
-  #ifdef ESP32
-    _webServer = new WebServer(80);
-  #else
-    _webServer = new ESP8266WebServer(80);
-  #endif
-};
-
-void HandleWebpage::handleRoot() {                         // When URI / is requested, send a web page with a button to toggle the LED
+void HandleWebpage::handleRoot() {  // When URI / is requested, send a web page
+                                    // with a button to toggle the LED
   handleWebRequests();
 }
 
-void HandleWebpage::handleSetLed(){
+void HandleWebpage::handleSetLed() {
   Serial.println("handleSetGain: " + _webServer->arg("plain"));
-  
-  if(_webServer->hasArg("led"))
-  {
+
+  if (_webServer->hasArg("led")) {
     String ledValue = _webServer->arg("led");
     Serial.printf("ledValue: %s\n", ledValue.c_str());
-    if(ledValue == "true")
-    {
-      #ifdef ESP32
-          digitalWrite(LED_BUILTIN, HIGH);
-      #else
-          digitalWrite(LED_BUILTIN, LOW);
-      #endif
+    if (ledValue == "true") {
+#ifdef ESP32
+      digitalWrite(LED_BUILTIN, HIGH);
+#else
+      digitalWrite(LED_BUILTIN, LOW);
+#endif
+    } else {
+#ifdef ESP32
+      digitalWrite(LED_BUILTIN, LOW);
+#else
+      digitalWrite(LED_BUILTIN, HIGH);
+#endif
     }
-    else
-    {
-      #ifdef ESP32
-          digitalWrite(LED_BUILTIN, LOW);
-      #else
-          digitalWrite(LED_BUILTIN, HIGH);
-      #endif
-    }
-    
+
     _webServer->send(200, "text/plane", "{\"success\": true}");
-  }
-  else
-  {
+  } else {
     Serial.println("Error handleSetLed: missing argument led!");
     _webServer->send(200, "text/plane", "{\"success\": false}");
   }
 }
 
-
-void HandleWebpage::handleWebRequests(){
-  if(!loadFromLittleFS(_webServer->uri()))
-  {
+void HandleWebpage::handleWebRequests() {
+  if (!loadFromLittleFS(_webServer->uri())) {
     Serial.println("Error: handleWebRequests");
     String message = "File Not Detected\n\n";
     message += "URI: ";
     message += _webServer->uri();
     message += "\nMethod: ";
-    message += (_webServer->method() == HTTP_GET)?"GET":"POST";
+    message += (_webServer->method() == HTTP_GET) ? "GET" : "POST";
     message += "\nArguments: ";
     message += _webServer->args();
     message += "\n";
-    for (uint8_t i=0; i < _webServer->args(); i++){
-      message += " NAME:" + _webServer->argName(i) + "\n VALUE:" + _webServer->arg(i) + "\n";
+    for (uint8_t i = 0; i < _webServer->args(); i++) {
+      message += " NAME:" + _webServer->argName(i) +
+                 "\n VALUE:" + _webServer->arg(i) + "\n";
     }
     _webServer->send(404, "text/plain", message);
-    //Serial.println(message);
+    // Serial.println(message);
   }
 }
 
-bool HandleWebpage::loadFromLittleFS(String path){
+bool HandleWebpage::loadFromLittleFS(String path) {
   bool returnValue = true;
   Serial.println("Load path: " + path);
   String dataType = "text/plain";
 
-  if(path.endsWith("/")) path += "index.html";
- 
-  if(path.endsWith(".src")) path = path.substring(0, path.lastIndexOf("."));
-  else if(path.endsWith(".html")) dataType = "text/html";
-  else if(path.endsWith(".htm")) dataType = "text/html";
-  else if(path.endsWith(".css")) dataType = "text/css";
-  else if(path.endsWith(".js")) dataType = "application/javascript";
-  else if(path.endsWith(".png")) dataType = "image/png";
-  else if(path.endsWith(".gif")) dataType = "image/gif";
-  else if(path.endsWith(".jpg")) dataType = "image/jpeg";
-  else if(path.endsWith(".ico")) dataType = "image/x-icon";
-  else if(path.endsWith(".xml")) dataType = "text/xml";
-  else if(path.endsWith(".pdf")) dataType = "application/pdf";
-  else if(path.endsWith(".zip")) dataType = "application/zip";
-  if (MYFS.exists(path))
-  {
+  if (path.endsWith("/")) path += "index.html";
+
+  if (path.endsWith(".src"))
+    path = path.substring(0, path.lastIndexOf("."));
+  else if (path.endsWith(".html"))
+    dataType = "text/html";
+  else if (path.endsWith(".htm"))
+    dataType = "text/html";
+  else if (path.endsWith(".css"))
+    dataType = "text/css";
+  else if (path.endsWith(".js"))
+    dataType = "application/javascript";
+  else if (path.endsWith(".png"))
+    dataType = "image/png";
+  else if (path.endsWith(".gif"))
+    dataType = "image/gif";
+  else if (path.endsWith(".jpg"))
+    dataType = "image/jpeg";
+  else if (path.endsWith(".ico"))
+    dataType = "image/x-icon";
+  else if (path.endsWith(".xml"))
+    dataType = "text/xml";
+  else if (path.endsWith(".pdf"))
+    dataType = "application/pdf";
+  else if (path.endsWith(".zip"))
+    dataType = "application/zip";
+  if (MYFS.exists(path)) {
     File dataFile = MYFS.open(path.c_str(), "r");
     if (_webServer->hasArg("download")) dataType = "application/octet-stream";
 
     if (_webServer->streamFile(dataFile, dataType) != dataFile.size()) {
-      //Serial.println("Error: streamed file has different size!");
-      //returnValue = false;
+      // Serial.println("Error: streamed file has different size!");
+      // returnValue = false;
     }
     dataFile.close();
-  }
-  else
-  {
+  } else {
     Serial.println("Error: Path does not exist and will be redirect to root:");
     Serial.println(path);
     returnValue = loadFromLittleFS("/");
@@ -115,25 +134,22 @@ bool HandleWebpage::loadFromLittleFS(String path){
   return returnValue;
 }
 
-void HandleWebpage::setupHandleWebpage()
-{
-    // replay to all requests with same HTML
+void HandleWebpage::setupHandleWebpage() {
+  // replay to all requests with same HTML
   _webServer->onNotFound(std::bind(&HandleWebpage::handleWebRequests, this));
 
   // replay to all requests with same HTML
-  //Information about using std::bind -> https://github.com/esp8266/Arduino/issues/1711
+  // Information about using std::bind ->
+  // https://github.com/esp8266/Arduino/issues/1711
 
   _webServer->on("/", HTTP_GET, std::bind(&HandleWebpage::handleRoot, this));
-  _webServer->on("/setLed", HTTP_POST, std::bind(&HandleWebpage::handleSetLed, this));
+  _webServer->on("/setLed", HTTP_POST,
+                 std::bind(&HandleWebpage::handleSetLed, this));
   _webServer->begin();
 }
 
-void HandleWebpage::sendSuccess()
-{
+void HandleWebpage::sendSuccess() {
   _webServer->send(200, "text/plane", "{\"success\": true}");
 }
 
-void HandleWebpage::handleClient()
-{
-  _webServer->handleClient();
-}
+void HandleWebpage::handleClient() { _webServer->handleClient(); }
