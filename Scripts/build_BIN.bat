ebdb34e2318d8c2ad9f71f97e7c6effee02b64f8
hredan <hredan@sourcecode3d.de>
2021-10-27 20:53:08 +0200

    update to Arduino 1.8.16

--- Scripts/build_BIN.bat
+++ Scripts/build_BIN.bat
@@ -6,10 +6,36 @@ set BUILD_PATH=%LOCALPATH%..\build
 set BUILD_CACHE_PATH=%LOCALPATH%..\cache
 set HOME=%HOMEDRIVE%%HOMEPATH%
 
+set GCC_VERSION=3.0.4-gcc10.3-1757bed
+
+set BUILD_PARAMETER=-logger=machine -hardware %ARDUINO_PATH%\hardware^
+ -hardware %HOME%\AppData\Local\Arduino15\packages^
+ -tools %ARDUINO_PATH%\tools-builder^
+ -tools %ARDUINO_PATH%\hardware\tools\avr^
+ -tools %HOME%\AppData\Local\Arduino15\packages^
+ -built-in-libraries %ARDUINO_PATH%\libraries^
+ -libraries %HOME%\Documents\Arduino\libraries^
+ -fqbn=esp8266:esp8266:d1_mini:xtal=160,vt=flash,exception=disabled,stacksmash=disabled^
+,ssl=all,mmu=3232,non32xfer=fast,eesz=4M2M,ip=hb2f,dbg=Disabled,lvl=None____,wipe=none,baud=921600^
+ -ide-version=10816 -build-path %BUILD_PATH% -warnings=none -build-cache %BUILD_CACHE_PATH% -prefs=build.warn_data_percentage=75^
+ -prefs=runtime.tools.python3.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\python3\3.7.2-post1^
+ -prefs=runtime.tools.python3-3.7.2-post1.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\python3\3.7.2-post1^
+ -prefs=runtime.tools.mklittlefs.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\mklittlefs\%GCC_VERSION%^
+ -prefs=runtime.tools.mklittlefs-%GCC_VERSION%.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\mklittlefs\%GCC_VERSION%^
+ -prefs=runtime.tools.mkspiffs.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\mkspiffs\%GCC_VERSION%^
+ -prefs=runtime.tools.mkspiffs-%GCC_VERSION%.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\mkspiffs\%GCC_VERSION%^
+ -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\xtensa-lx106-elf-gcc\%GCC_VERSION%^
+ -prefs=runtime.tools.xtensa-lx106-elf-gcc-%GCC_VERSION%.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\xtensa-lx106-elf-gcc\%GCC_VERSION%^
+ -verbose D:\Freiiiiheit\GIT\ESP_jQuery_Mobile_Interface\ESP_jQuery_Mobile_Interface.ino
+
 mkdir %BUILD_PATH%
 mkdir %BUILD_CACHE_PATH%
 
-%ARDUINO_PATH%\arduino-builder -dump-prefs -logger=machine -hardware %ARDUINO_PATH%\hardware -hardware %HOME%\AppData\Local\Arduino15\packages -tools %ARDUINO_PATH%\tools-builder -tools %ARDUINO_PATH%\hardware\tools\avr -tools %HOME%\AppData\Local\Arduino15\packages -built-in-libraries %ARDUINO_PATH%\libraries -libraries %HOME%\Documents\Arduino\libraries -fqbn=esp8266:esp8266:d1_mini:xtal=160,vt=flash,exception=legacy,ssl=all,eesz=4M2M,ip=hb2f,dbg=Disabled,lvl=None____,wipe=none,baud=921600 -ide-version=10813 -build-path %BUILD_PATH% -warnings=none -build-cache %BUILD_CACHE_PATH% -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.python3.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\python3\3.7.2-post1 -prefs=runtime.tools.python3-3.7.2-post1.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\python3\3.7.2-post1 -prefs=runtime.tools.mklittlefs.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\mklittlefs\2.5.0-4-fe5bb56 -prefs=runtime.tools.mklittlefs-2.5.0-4-fe5bb56.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\mklittlefs\2.5.0-4-fe5bb56 -prefs=runtime.tools.mkspiffs.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\mkspiffs\2.5.0-4-b40a506 -prefs=runtime.tools.mkspiffs-2.5.0-4-b40a506.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\mkspiffs\2.5.0-4-b40a506 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\xtensa-lx106-elf-gcc\2.5.0-4-b40a506 -prefs=runtime.tools.xtensa-lx106-elf-gcc-2.5.0-4-b40a506.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\xtensa-lx106-elf-gcc\2.5.0-4-b40a506 -verbose D:\Freiiiiheit\GIT\ESP_jQuery_Mobile_Interface\ESP_jQuery_Mobile_Interface.ino
-%ARDUINO_PATH%\arduino-builder -compile -logger=machine -hardware %ARDUINO_PATH%\hardware -hardware %HOME%\AppData\Local\Arduino15\packages -tools %ARDUINO_PATH%\tools-builder -tools %ARDUINO_PATH%\hardware\tools\avr -tools %HOME%\AppData\Local\Arduino15\packages -built-in-libraries %ARDUINO_PATH%\libraries -libraries %HOME%\Documents\Arduino\libraries -fqbn=esp8266:esp8266:d1_mini:xtal=160,vt=flash,exception=legacy,ssl=all,eesz=4M2M,ip=hb2f,dbg=Disabled,lvl=None____,wipe=none,baud=921600 -ide-version=10813 -build-path %BUILD_PATH% -warnings=none -build-cache %BUILD_CACHE_PATH% -prefs=build.warn_data_percentage=75 -prefs=runtime.tools.python3.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\python3\3.7.2-post1 -prefs=runtime.tools.python3-3.7.2-post1.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\python3\3.7.2-post1 -prefs=runtime.tools.mklittlefs.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\mklittlefs\2.5.0-4-fe5bb56 -prefs=runtime.tools.mklittlefs-2.5.0-4-fe5bb56.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\mklittlefs\2.5.0-4-fe5bb56 -prefs=runtime.tools.mkspiffs.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\mkspiffs\2.5.0-4-b40a506 -prefs=runtime.tools.mkspiffs-2.5.0-4-b40a506.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\mkspiffs\2.5.0-4-b40a506 -prefs=runtime.tools.xtensa-lx106-elf-gcc.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\xtensa-lx106-elf-gcc\2.5.0-4-b40a506 -prefs=runtime.tools.xtensa-lx106-elf-gcc-2.5.0-4-b40a506.path=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\xtensa-lx106-elf-gcc\2.5.0-4-b40a506 -verbose D:\Freiiiiheit\GIT\ESP_jQuery_Mobile_Interface\ESP_jQuery_Mobile_Interface.ino
+set 
 
+REM DUMP
+%ARDUINO_PATH%\arduino-builder -dump-prefs %BUILD_PARAMETER%
+
+REM COMPILE
+%ARDUINO_PATH%\arduino-builder -compile %BUILD_PARAMETER%
 pause
--- Scripts/createLitteFS.bat
+++ Scripts/createLitteFS.bat
@@ -5,6 +5,6 @@ set BUILD_PATH=%LOCALPATH%..\build
 set HOME=%HOMEDRIVE%%HOMEPATH%
 
 mkdir %BUILD_PATH%
-set TOOL_PATH=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\mklittlefs\2.5.0-4-fe5bb56\mklittlefs.exe
+set TOOL_PATH=%HOME%\AppData\Local\Arduino15\packages\esp8266\tools\mklittlefs\3.0.4-gcc10.3-1757bed\mklittlefs.exe
 %TOOL_PATH% -c ../data -p 256 -b 8192 -s 2072576 %BUILD_PATH%\littlefs.bin
 pause
