18cb92b01a9189b7bc1c6434ae6ffe3482fabafe
hredan <hredan@sourcecode3d.de>
2021-11-02 21:25:06 +0100

    add scripts to zip raw data

--- Scripts/run_all.sh
+++ Scripts/run_all.sh
@@ -5,3 +5,7 @@ git clean -xdf
 SKETCH_NAME=ESP_jQuery_Mobile_Interface
 #create data bin
 sh ./Scripts/build_data.sh $SKETCH_NAME
+sh ./Scripts/build_sketch.sh ESP8266
+sh ./Scripts/build_sketch.sh ESP32
+sh ./Scripts/save_raw_data.sh
+
