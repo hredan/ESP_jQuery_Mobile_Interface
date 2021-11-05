a92c0075349dbc6866eac9326c47a3636060ca63
hredan <hredan@sourcecode3d.de>
2021-11-05 07:41:38 +0100

    update eef_package script but not final

--- Scripts/create_eef_package.sh
+++ Scripts/create_eef_package.sh
@@ -10,8 +10,11 @@ CHECK_ESP32=0
 CHECK_DATA=0
 
 ESPTOOL_PARA_ESP8266="\"--baud\", \"460800\", \"write_flash\", \"0x0\", \"${1}.ino.bin\""
-ESPTOOL_PARA_ESP32=""
 ESPTOOL_PARA_FS=", \"0x200000\" ${1}_littlefs.bin"
+ESPTOOL_PARA_ESP32="\"command\": [\"--chip\", \"esp32\", \"--baud\", \"921600\", \"--before\", \"default_reset\", \"--after\", \"hard_reset\", \"write_flash\", \"-z\", \"--flash_mode\", \"dio\", \"--flash_freq\", \"80m\", \"--flash_size\", \"detect\""
+ESPTOOL_PARA_ESP32_FILES=", \"0xe000\", \"boot_app0.bin\", \"0x1000\", \"ESP32_jQuery_Mobile_Interface.ino.bootloader.bin\", \"0x10000\", \"ESP32_jQuery_Mobile_Interface.ino.bin\", \"0x8000\", \"ESP32_jQuery_Mobile_Interface.ino.partitions.bin\""
+ESPTOOL_PARA_ESP32_FS", \"0x210000\", \"${1}_littlefs.bin\""
+
 
 if [ -d $ESP8266_BUILD_DIR ]
 	then
@@ -52,10 +55,13 @@ if [ $CHECK_DATA -eq 1 ]; then
 fi
 
 if [ $CHECK_ESP8266 -eq 1 ]; then
+	ESP8266_EEF_PATH=$EEP_DIR/ESP8266_$1.eef
 	cp ${ESP8266_BUILD_DIR}/${1}.ino.bin ${EEP_DIR}/ESP8266_${1}.ino.bin
 	if [ $CHECK_DATA -eq 0 ]; then
-		echo "{\n\t\"command\": [${ESPTOOL_PARA_ESP8266}]\n}" > $EEP_DIR/$1.eef
+		echo "{\n\t\"command\": [${ESPTOOL_PARA_ESP8266}]\n}" > $ESP8266_EEF_PATH
 	else
-		echo "{\n\t\"command\": [${ESPTOOL_PARA_ESP8266}${ESPTOOL_PARA_FS}]\n}" > $EEP_DIR/$1.eef
+		echo "{\n\t\"command\": [${ESPTOOL_PARA_ESP8266}${ESPTOOL_PARA_FS}]\n}" > $ESP8266_EEF_PATH
 	fi
 fi
+
+
