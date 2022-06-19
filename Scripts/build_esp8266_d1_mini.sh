0655ee3926f718b9adfe34069af8d3c00a42ec9b
hredan <hredan@sourcecode3d.de>
2022-06-19 06:57:39 +0200

    add release workflow

---
+++ Scripts/build_esp8266_d1_mini.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SCRIPT_DIR=${0%/*}
+SKETCH_NAME=ESP_jQuery_Mobile_Interface
+CORE=esp8266
+BOARD=d1_mini
+CPUF=80
+
+SCRIPT_DIR=${0%/*}
+
+EEP_DIR=${SCRIPT_DIR}/../EEP
+if [ -d ${EEP_DIR} ]; then
+    rm -r ${EEP_DIR}
+fi
+
+# build ESP binaries
+bash ${SCRIPT_DIR}/../ESP_Build_Scripts/build_sketch.sh -s ${SKETCH_NAME} -c ${CORE} -b ${BOARD} -f ${CPUF}
+
+# create zip file (eef package)
+if [ -d ${EEP_DIR} ]; then
+     zip -j ${CORE}_${BOARD}_${SKETCH_NAME}.eep ${EEP_DIR}/*
+else
+    echo "Error could not find directory "
+fi
